1f199081d322ead256cc7a7a9bf4016e13f778bd
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-12 20:32:45 +0000

    Automatically backed up by Learn

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -11,6 +11,8 @@ class Genre < ActiveRecord::Base
   end
 
   def all_artist_names
-    # return an array of strings containing every musician's name
+       self.artists.collect do |t|
+      t.name
+    end
   end
 end
